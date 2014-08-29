92f72c1b7cfd2926bc2395c7ed99280c6f45c66a
sunshine-eunice <sunshineeunicec@yahoo.com>
2014-08-29 22:07:44 +0800

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,125 +1,232 @@
-$(function() {
-    // your code here
-    var config;
-    var baseUrl = 'http://api.themoviedb.org/3/',
-        apiKey = '0a92b9230aa4a56d3f4157df21755d11';
-
-
-    function initialize(callback) {
-        $.get(baseUrl + 'configuration', {
-            api_key: '0a92b9230aa4a56d3f4157df21755d11'
-        },function(res) {
-            config = res;
-            console.log(config);
-            callback(config);
+var apiKey = "0a92b9230aa4a56d3f4157df21755d11";
+var baseUrl = "http://api.themoviedb.org/3";
+var prevQuery;
+var functionToCall;
+var totalPages;
+var hist=new Array();
+var firstBack=true;
+function init(){
+    $(document).ready(function() {
+        getMovies("popular",1,assignPage);
+        $("#movie-search").submit(function(){
+            var val = $("#movie-search input").val();
+            searchMovie(val,1,assignPage);
+            showResults();
+            return false;
         });
+        $(".filter-link").click(function(){
+            var query = $(this).attr("id");
+            getMovies(query,1,assignPage);
+            showResults();
+        });
+        $(".similar-toggle").click(function(){
+            $("#similar-movies2").toggle();
+            $(".similar-toggle").toggle();  
+
+        });
+        $("#movie-view").hide();
+        $("#back").click(goBack);
+    });
+}
+function formatMovieHTML(movie){
+    //make an html layout for a movie
+    var imageUrl = 'https://image.tmdb.org/t/p/w130/';
+    if(movie.original_title.length>40){
+        movie.original_title = movie.original_title.substr(0,40)+"..";
     }
-
-    function setEventHandlers(config) {
-        $('#form-search').submit(function() {
-            var query = $('.input-query').val();
-            searchMovie(query);
-            return  false;
-        });
-
-        $('.btn-now-showing').click(function() {
-            loadNowShowing();
-            return  false;
-        });
-
-        loadNowShowing();
-
-        $('.btn-upcoming').click(function() {
-            loadUpcoming();
-            return  false;
-        });
-
-        loadUpcoming();
-
-        $('.btn-popular').click(function() {
-            loadPopular();
-            return  false;
-        });
-
-        loadPopular();
-
-        $('.btn-top-rated').click(function() {
-            loadTopRated();
-            return  false;
-        });
-
-        loadTopRated();
-
+    if(!movie.poster_path){
+        imageUrl = "/static/img/movie.jpg";
+    }else{
+        imageUrl +=movie.poster_path;
     }
-
-    function searchMovie(query) {
-        var searchUrl = baseUrl + 'search/movie';
-        $('.movies-list').html('');
-        $.get(searchUrl, {
-            query: query,
-            api_key: apiKey
-        }, function(response) {
-            displayMovies(response);
-        });
+    var html = '<div class="col-md-3 portfolio-item" data-id="'+movie.id+'">'+
+        '<a href="#">'+
+            '<img height="100px"class="img-responsive" src="'+imageUrl+'" alt="/static/img/logo.png">'+
+        '</a>'+
+        '<h4>'+
+        '    <a href="#" >'+movie.original_title+'</a>'+
+        '</h4>'+
+    '</div>';
+    return html;
+}
+function makeEmbed(key){
+    return '<embed width="500" height="400" src="https://www.youtube.com/v/'+key+'" type="application/x-shockwave-flash">'
+}
+function createMovieLink(movie){
+    movie.title = (movie.title.length<1)? movie.original_title : movie.title;
+    var html = '<a href="#" data-id="'+movie.id+'" class="similar-click">'+movie.title+'</a>';
+    return html;
+}
+function searchMovie(query, page, callback){
+    prevQuery = query;
+    page = (page>1000)?1000:page;
+    functionToCall = searchMovie;
+    var url = baseUrl + "/search/movie";
+    var reqParam = {
+        api_key : apiKey,
+        query : query,
+        page : page
     }
-
-    function displayMovies(data) {
-        data.results.forEach(function(movie) {
-            var imageSrc = config.images.base_url + config.images.poster_sizes[3] + movie.poster_path;
-            var htmlStr = [
-                            '<div class="col-md-4 portfolio-item">',
-                                '<a href="#">',
-                                    '<img class="img-responsive" src="' + imageSrc + '" alt="">',
-                                '</a>',
-                                '<h3>',
-                                    '<a href="#">' + movie.title +'</a>',
-                                '</h3>',
-                            '</div>'
-                            ];
-            $('.movies-list').append($(htmlStr.join('')));
-        });
+    $.get(url,reqParam,callback);
+    $(".page-header").html("Search Results");
+}
+function getMovies(query, page, callback){
+    var url = baseUrl +"/movie/" +query;
+    functionToCall = getMovies;
+    prevQuery = query;
+    var reqParam = {
+        api_key : apiKey,
+        page : page
     }
-
-    function loadNowShowing() {
-        var nowShowingUrl = baseUrl + 'movie/now_playing';
-        $('.movies-list').html('');
-        $.get(nowShowingUrl, {
-            api_key: apiKey
-        }, function(response) {
-            displayMovies(response);
-        });
-    }
-
-    function loadUpcoming() {
-        var upcomingUrl = baseUrl + 'movie/upcoming';
-        $('.movies-list').html('');
-        $.get(upcomingUrl, {
-            api_key: apiKey
-        }, function(response) {
-            displayMovies(response);
-        });
-    }
-
-    function loadPopular() {
-        var popularUrl = baseUrl + 'movie/popular';
-        $('.movies-list').html('');
-        $.get(popularUrl, {
-            api_key: apiKey
-        }, function(response) {
-            displayMovies(response);
-        });
-    }
-
-    function loadTopRated() {
-        var topRatedUrl = baseUrl + 'movie/top_rated';
-        $('.movies-list').html('');
-        $.get(topRatedUrl, {
-            api_key: apiKey
-        }, function(response) {
-            displayMovies(response);
-        });
-    }
+    $.get(url,reqParam,callback);
+    $(".page-header").html($("#"+query).html()+" Movies");
+}
+function assignPage(result){
     
-    initialize(setEventHandlers);
+    $(".pagination").html("");
+    
+    totalPages = result.total_pages;
+    var startingPage = (result.page<4)? 1:result.page-3;
+    var endPage = (result.total_pages-result.page>7)? startingPage+7: totalPages;
+    endPage = (endPage>1000)?1000:endPage;
+    var html = '<li><a href="#" onclick="goToPage(1)">&laquo;</a></li>';
+    $(".pagination").append(html);
+    
+    for(var i =startingPage;i<=endPage;i++){
+        var html = (result.page==i)?'<li class="active"><span>'+i+'<span class="sr-only">(current)</span></span></li>'
+            :'<li><a href="#" class="page-click">'+i+'</a></li>';
+        $(".pagination").append(html);
+    }
 
-});
+    var html = '<li><a href="#" onclick="goToPage(0)">&raquo;</a></li>';
+    $(".pagination").append(html);
+    var b = (result.page ==1)? 0:result.page-1;
+    var a = (result.page==totalPages || totalPages==0)?result.total_results:(totalPages>0)?b*20+20:0;
+    
+    $("#page-number").html(b*20+"-"+a+" out of "+ result.total_results);
+    
+    displayMovies(result);
+    $(".page-click").click(function(callback){
+        var pageToSearch = $(this).text();
+        functionToCall(prevQuery,pageToSearch,assignPage);
+    });
+}
+function goToPage(pageToSearch){
+    totalPages=(totalPages>1000)?1000:totalPages;
+    pageToSearch = (pageToSearch==0)? totalPages : 1;
+
+    functionToCall(prevQuery,pageToSearch,assignPage);
+}
+function displayMovies(result){
+    $("#movie-list").html("");
+    var movies = result.results;
+    if(movies.length<1){
+            $("#movie-list").html("<h2>No Result Found</h2>");
+    }
+    var movieFormatHTML;
+    for(var i=0; i<movies.length;i++){
+        movieFormatHTML = formatMovieHTML(movies[i]);
+        var html = (i%4>0)?movieFormatHTML: '<div style="margin-top:10px;"></div>'+movieFormatHTML;
+        $("#movie-list").append(html);
+    }
+    $(".col-md-3").click(movieClick);
+}
+function queryMovie(id){
+    url = baseUrl + "/movie/"+id;
+
+    reqParam = {api_key:apiKey};
+    $.get(url,reqParam,showMovie);
+}
+function showMovie(result){
+    showViewPage();
+    $(".page-header").text(result.original_title);
+    var synopsis = result.overview;
+    $("#synopsis").text(synopsis);
+    var url = baseUrl + "/movie/"+result.id+"/videos";
+    reqParam = {api_key:apiKey};
+    $.get(url,reqParam,showVideos);
+    url = baseUrl + "/movie/"+result.id+"/credits";
+    $.get(url,reqParam,showCasts);
+    url = baseUrl + "/movie/"+result.id+"/similar";
+    $.get(url,reqParam,showSimiralMovies);
+}
+function showVideos(result){
+    result = result.results;
+    var youtubeUrl = "https://www.youtube.com/v/";
+    var embed;
+    if(result.length>1){
+        embed = makeEmbed(result[0].key);
+        $("#Trailer1").html(embed);
+        embedUrl = youtubeUrl + result[1].key;
+        $("#Trailer2").html(embed);
+        $("[href = '#Trailer2'").show();    
+    }else if(result.length>0){
+        embed = makeEmbed(result[0].key);
+        $("#Trailer1").html(embed);
+        $("Trailer1").tab("show");
+        $("[href = '#Trailer2'").hide();    
+    }else{
+        embed = "<br>No Trailer Available"
+        $("#Trailer1").html(embed);
+        $("Trailer1").tab("show");
+        $("[href = '#Trailer2'").hide();
+    }
+}
+function showCasts(result){
+    var casts="";
+    for(var i=0;i<result.cast.length;i++){
+        casts+= (i!=result.cast.length-1)?result.cast[i].name+", "
+            : " and "+result.cast[i].name;
+    }
+    $("#casts").html(casts);
+}
+function showSimiralMovies(result){
+    var similarMovies = result.results;
+    var movies;
+    $("#similar-movies1").html("");
+    $("#similar-movies2").html("");
+    for(var i=0;i<similarMovies.length;i++){
+        movies=(i==3)?createMovieLink(similarMovies[i])
+            :(i!=similarMovies.length-1)?createMovieLink(similarMovies[i])+", "
+            :createMovieLink(similarMovies[i]);
+        if(i<4){
+            $("#similar-movies1").append(movies);
+        }else{
+            $("#similar-movies2").append(movies);
+        }
+    }
+    $(".similar-click").click(function(){
+        var id=$(this).attr("data-id");
+        hist.push(id);
+        firstBack=true;
+        queryMovie(id);
+
+    }); 
+}
+function movieClick(){
+    var id = $(this).attr("data-id");
+    queryMovie(id);
+    hist.push(id);
+    firstBack=true;
+}
+function goBack(a){
+    if(firstBack){
+        hist.pop();
+    }
+    if(hist.length<1){
+        showResults();
+    }else{
+        var id = hist.pop();
+        console.log(id)
+        queryMovie(id);
+    }
+    firstBack=false;
+}
+function showViewPage(){
+    $("#movie-list, #page-number,.pagination").hide();
+    $("#movie-view").show();
+}
+function showResults(){
+    $("#movie-list, #page-number,.pagination").show();
+    $("#movie-view").hide();
+}
